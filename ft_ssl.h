9f179b360ee7a648b763fd7d0bec6c46c2ddfe6e
Boris Goncharov <bora4bora@gmail.com>
2019-07-22 18:27:17 -0700

    md5 and small string

--- ft_ssl.h
+++ ft_ssl.h
@@ -6,7 +6,7 @@
 /*   By: bogoncha <bogoncha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/16 16:54:46 by bogoncha          #+#    #+#             */
-/*   Updated: 2019/07/20 19:53:58 by bogoncha         ###   ########.fr       */
+/*   Updated: 2019/07/22 18:26:09 by bogoncha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,18 @@ unsigned		fun_i(unsigned x, unsigned y, unsigned z);
 typedef struct	s_fmd5
 {
 	unsigned	hash[8];
-	unsigned	bits[2];
 	int			len;
 	int			bitlen;
-}   			t_fmd5;
+}				t_fmd5;
 
+void			md5_init(t_fmd5 *fmd, char *str);
+unsigned char 	*md5_update(t_fmd5 *fmd, char *str);
+unsigned		rotate(unsigned a, unsigned b);
 void			stage_one(t_fmd5 *fmd, unsigned *x);
 void			stage_two(t_fmd5 *fmd, unsigned *x);
 void			stage_three(t_fmd5 *fmd, unsigned *x);
 void			stage_four(t_fmd5 *fmd, unsigned *x);
+unsigned		*md5_final(t_fmd5 *fmd);
+void			print_md5(unsigned hash[]);//temp
 
 #endif
